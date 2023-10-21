bba5d0d9d225c3c600e1cf82d99c2869a08a1992
Agwenyi Ogata <angwenyiamos01@gnail.com>
2023-10-21 08:48:35 +0300

    modified css

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* const dropDown = document.querySelectorAll(
+  '.header__section_one nav  ul ol h1 img'
+);
+
+// console.log(dropDown);
+const hidden = document.querySelectorAll('.header__section_one nav  ul ol li');
+/* dropDown.addEventListener('click', () => {
+  alert('clicked');
+  console.log(dropDown);
+}); 
+
+for (let i = 0; i < dropDown.length; i++) {
+  dropDown[i].addEventListener('click', () => {
+    
+    for (let i = 0; i < hidden.length; i++) {
+      hidden[i].style.display = 'block';
+    }
+  });
+} */
